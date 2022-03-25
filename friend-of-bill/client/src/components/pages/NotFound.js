7f7a4718578b86abdf171d7490d10dc726c83545
cjward2 <christopher.ward754@gmail.com>
2022-03-25 11:21:38 -0500

    added styling to 404 page

--- friend-of-bill/client/src/components/pages/NotFound.js
+++ friend-of-bill/client/src/components/pages/NotFound.js
@@ -1,14 +1,16 @@
 import { Link } from 'react-router-dom';
 import requireAuth from '../requireAuth';
+import './NotFound.css';
 
 const NotFound = () => {
   return (
     <div className='not-found'>
-        <h1 className='not-found__title'>Waa Waa Wahhh</h1>
-        <p className="not-found__desc">
-        Sorry it looks like this page doesn't exist. You can return to the dashboard <Link className='not-found__desc--link' to="/dashboard">Here</Link>
-        </p>
-        
+        <div className="not-found__content">
+          <h1 className='not-found__title'>Waa Waa Wahhh</h1>
+          <p className="not-found__desc">
+          Sorry it looks like this page doesn't exist. You can return to the dashboard <Link className='not-found__desc--link' to="/dashboard">Here</Link>
+          </p>
+        </div> 
     </div>
   )
 }
