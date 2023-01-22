bfa3da85432b28595b0a728807fa7b45847c8995
Chris  Ward <84754656+cjward2@users.noreply.github.com>
2023-01-21 22:35:15 -0600

    feat: :construction: project intialized -> added header and hero

---
+++ personal-website/components/Header.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center"
+      >
+        <SocialIcon
+          url="https://www.youtube.com"
+          fgColor="gray"
+          bgColor="transparent"
+          target="_blank"
+        />
+        <SocialIcon
+          url="https://www.linkedin.com/in/christopher-ward-040b26212/"
+          fgColor="gray"
+          bgColor="transparent"
+          target="_blank"
+        />
+        <SocialIcon
+          url="https://github.com/cjward2/Portfolio"
+          fgColor="gray"
+          bgColor="transparent"
+          target="_blank"
+        />
+      </motion.div>
+
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
+        <SocialIcon network="email" fgColor="gray" bgColor="transparent" />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Get in touch!
+        </p>
+      </motion.div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ personal-website/pages/index.tsx
@@ -0,0 +1,19 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+      <Head>
+        <title>Chris Ward Portfolio</title>
+      </Head>
+
+      <Header />
+
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
+    </div>
+  );
+}
